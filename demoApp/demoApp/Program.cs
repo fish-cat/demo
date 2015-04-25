18fccdc4c2d075f052605b7f56a6a1a22f7841f5
Daphne Ng <daphnentw@hotmail.com>
2015-04-25 14:48:00 -0400

    Added UC message

---
+++ demoApp/demoApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UC 2015!");
+            Console.ReadLine();
+        }
+    }
+}
